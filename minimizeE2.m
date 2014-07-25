ccdf6c2440fad25df0ccee841da034d5c407ca0c
Ryan Lowe <ryan.lowe_@hotmail.com>
2014-07-25 14:25:48 -0400

    changes to 2-site code
    unable to generate correct matrix sizes post svd

--- minimizeE2.m
+++ minimizeE2.m
@@ -1,4 +1,4 @@
-function [E,mps]=minimizeE2(mpo,D,precision,mpsB)
+function [E,mps]=minimizeE2(mpo,D,precision)
 
 N = size(mpo,2); 
 d = 2; 
@@ -7,7 +7,6 @@ mps = prepare(mps,'rl');
 
 % storage-initialization
 Hstorage = initHstorage(mps,mpo,d);
-if ~isempty(mpsB), Cstorage = initCstorage(mps,[],mpsB,N); end
 
 % optimization sweeps 
 while 1
@@ -15,24 +14,22 @@ while 1
 
     % ****************** cycle 1: j -> j+1 (from 1 to N-1) **************** 
     for j = 1:(N-1)
-        % projector-calculation 
-        if ~isempty(mpsB)
-            B = mpsB{j};
-            Cleft = Cstorage{j};
-            Cright = Cstorage{j+1}; 
-        end
-
         % optimization
         Hleft = Hstorage{j};
         Hright = Hstorage{j+2};
         hsetj1 = mpo{j};
         hsetj2 = mpo{j+1};
-        hsetj=contracttensors(hsetj1,3,3,hsetj2,3,2);
-        DAl = size(Hleft,1); 
-        DAr = size(Hright,1);
-        HS1 = size(hsetj1,1);
-        hsetj=contracttensors(hsetj1,3,3,hsetj2,3,2);
-        hsetj=reshape(hsetj,[DAl,HS1*d,DAr]); %probably not right
+        
+        hsetj=contracttensors(hsetj1,4,2,hsetj2,4,1);
+        %sizeH1=size(hsetj)
+        hsetj=permute(hsetj,[1,4,2,3,5,6]);
+        HS1=size(hsetj,1);
+        HS2=size(hsetj,2);
+        %hsetj=reshape(hsetj,[HS1,HS2,4,4]); %probably not right
+        
+        sizem1=size(mps{1})
+        sizem2=size(mps{2})
+        
         [A1,A2,E] = minimizeE_twosites(hsetj,Hleft,Hright,'lr'); 
         [A1,U] = prepare_onesite(A1,'lr');
         mps{j} = A1; 
@@ -41,31 +38,25 @@ while 1
         Evalues = [Evalues,E];
  
         % storage-update 
-        Hstorage{j+1} = updateCleft(Hleft{m},A1,hsetj1,A1); 
-        if ~isempty(mpsB) 
-            Cstorage{j+1} = updateCleft(Cleft,A1,[],B);
-        end
+        Hstorage{j+1} = updateCleft(Hleft,A1,hsetj1,A1); 
     end
    
     % ****************** cycle 2: j -> j-1 (from N to 2) ****************** 
     for j = N:(-1):2
-        % projector-calculation 
-        if ~isempty(mpsB)
-            B = mpsB{j};
-            Cleft = Cstorage{j};
-            Cright = Cstorage{j+1}; 
-        end
+
         
         % minimization
         Hleft = Hstorage{j-1};
         Hright = Hstorage{j+1};
         hsetj1 = mpo{j};
         hsetj2 = mpo{j-1};
-        DAl = size(Hleft,1); 
-        DAr = size(Hright,1);
-        HS1 = size(hsetj1,1);
-        hsetj=contracttensors(hsetj1,3,3,hsetj2,3,2);
-        hsetj=reshape(hsetj,[DAl,HS1*d,DAr]); %probably not right
+        
+        hsetj=contracttensors(hsetj1,4,2,hsetj2,4,1);
+        hsetj=permute(hsetj,[1,4,2,3,5,6]);
+        HS1=size(hsetj,1);
+        HS2=size(hsetj,2);
+        hsetj=reshape(hsetj,[HS1,HS2,4,4]); %probably not right
+        
         [A1,A2,E] = minimizeE_twosites(hsetj,Hleft,Hright,'rl'); 
         [A1,U] = prepare_onesite(A1,'rl');
         mps{j} = A1; 
@@ -74,10 +65,8 @@ while 1
         Evalues = [Evalues,E];
 
         % storage-update 
-        Hstorage{j}=updateCright(Hright{m},A1,hsetj1,A1); 
-        if ~isempty(mpsB) 
-            Cstorage{j}=updateCright(Cright,A,[],B);
-        end
+        Hstorage{j}=updateCright(Hright,A1,hsetj1,A1); 
+
     end
     if (std(Evalues)/abs(mean(Evalues))<precision) 
         mps{1}=contracttensors(mps{1},3,2,U,2,1); 
--- minimizeE_twosites.m
+++ minimizeE_twosites.m
@@ -6,30 +6,56 @@ DAr = size(Hright,1);
 d = 2;
 
 % calculation of Heff
-
+sizeH=size(hsetj)
 % hsetj=contracttensors(hsetj1,3,3,hsetj2,3,2);
 % hsetj=reshape(hsetj,[
 
-Heff = 0; 
-Heff = contracttensors(Hleft,3,2,Hright,3,2); 
-Heff = contracttensors(Heff,5,5,hsetj,3,3); 
-Heff = permute(Heff,[1,3,5,2,4,6]); 
-Heff = reshape(Heff,[DAl*DAr*d,DAl*DAr*d]); 
+Heff = contracttensors(Hleft,3,2,hsetj,6,1); 
+Heff = contracttensors(Heff,7,3,Hright,3,2); 
+
+%sizeH=size(Heff)
+
+%Heff is initially in the arrangement [a1,a2,o1,o2,b1,b2]
+sizeHeff=size(Heff)
+Heff = permute(Heff,[1,7,3,4,2,8,5,6]); 
+Heff = reshape(Heff,[DAl*DAr*d*d,DAl*DAr*d*d]); 
+
 
 %
 % optimization
 %
-% options.disp = 0; 
+options.disp = 0; 
 [A1,E] = eigs(Heff,1,'sr',options); 
 
-[U,S,V]=svd(Heff);
+sizeA1=size(A1)
+A1=reshape(A1,[DAl*d,DAr*d]);
+[U,S,V]=svd(A1);
+
+
+%dimensions after doing svd were not correct for rearrangement, so
+%'inner dimension' constructed ( = D from original mpo)
+
+%probably isn't correct, though (and still doesn't work for boundaries)
+innerD=8;
+innerDU=min(innerD,size(U,2));
+innerDV=min(innerD,size(V,2));
+
+U=U(:,1:innerDU);
+S=S(1:innerDU,1:innerDV);
+V=V(:,1:innerDV);
+
+sizeU=size(U)
+sizeS=size(S)
+sizeV=size(V)
 if strcmp(dir,'lr')
-    A2=S*V';
+    A1=U;
+    A2=S*V';   
 else
+    A1=V';
     A2=U*S;
 end
 % A1=U;
 % A2=S*V';
 
-A1=reshape(A1,[DAl,DAr,d]);
-A2=reshape(A2,[DAl,DAr,d]);
+A1=reshape(A1,[DAl,innerD,d]);
+A2=reshape(A2,[innerD,DAr,d]);
